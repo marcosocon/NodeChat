7ec60bdcf852934cc9c5eee63cf53c5da6728697
marcosocon <oconmarcos97@gmail.com>
2016-03-14 18:09:03 -0300

    added chat functionality to send and receive messages

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-var express = require("express");
-var app = express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get("/", function(req,res){
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", function(socket){
-  console.log("a user connected");
-  socket.on("disconnect",function(){
-    console.log("a user disconnected");
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
 http.listen(3000, function(){
-  console.log("listening on port 3000!");
+  console.log('listening on *:3000');
 });
