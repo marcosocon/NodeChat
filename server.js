7b800baee0c851c42a30ad89bc8216b8c0ee25b1
marcosocon <oconmarcos97@gmail.com>
2016-03-14 14:56:18 -0300

    added socket io and added connect and disconnect events

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get("/", function(req,res){
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000,function(){
-  console.log("Listening in port 3000");
+io.on("connection", function(socket){
+  console.log("a user connected");
+  socket.on("disconnect",function(){
+    console.log("a user disconnected");
+  });
+});
+
+http.listen(3000, function(){
+  console.log("listening on port 3000!");
 });
